264731cc651b12d4d39ff216f8ef9a6fd13feeb5
carlosvin <carlosvin@gmail.com>
2014-03-12 20:40:01 +0100

    site was created

---
+++ blog/cache/.mako.tmp/crumbs.tmpl.py
@@ -0,0 +1,53 @@
+# -*- coding:utf-8 -*-
+from mako import runtime, filters, cache
+UNDEFINED = runtime.UNDEFINED
+__M_dict_builtin = dict
+__M_locals_builtin = locals
+_magic_number = 9
+_modified_time = 1394653098.0244029
+_enable_loop = True
+_template_filename = '/home/carlos/apps/virtualenvs/nikola/lib/python3.3/site-packages/nikola/data/themes/base/templates/crumbs.tmpl'
+_template_uri = 'crumbs.tmpl'
+_source_encoding = 'utf-8'
+_exports = ['bar']
+
+
+def render_body(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        __M_locals = __M_dict_builtin(pageargs=pageargs)
+        __M_writer = context.writer()
+        # SOURCE LINE 2
+        __M_writer('\n')
+        # SOURCE LINE 11
+        __M_writer('\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_bar(context,crumbs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        __M_writer = context.writer()
+        # SOURCE LINE 3
+        __M_writer('\n')
+        # SOURCE LINE 4
+        if crumbs:
+            # SOURCE LINE 5
+            __M_writer('<ul class="breadcrumb">\n')
+            # SOURCE LINE 6
+            for link, text in crumbs:
+                # SOURCE LINE 7
+                __M_writer('        <li><a href="')
+                __M_writer(str(link))
+                __M_writer('">')
+                __M_writer(str(text))
+                __M_writer('</a></li>\n')
+            # SOURCE LINE 9
+            __M_writer('</ul>\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
---
+++ blog/output/posts/fist-post.rst
@@ -0,0 +1,9 @@
+.. title: Fist post
+.. slug: fist-post
+.. date: 2014/03/12 20:37:59
+.. tags: 
+.. link: 
+.. description: 
+.. type: text
+
+Write your post here.
