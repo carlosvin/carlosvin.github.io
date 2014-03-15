ccb72c4b700ae94eb3a644c80357a0270b12acde
carlos <carlosvin@gmail.com>
2014-03-15 20:40:18 +0100

    new theme

--- conf.py
+++ conf.py
@@ -12,7 +12,7 @@ BLOG_AUTHOR = "Carlos Martín Sánchez"
 BLOG_TITLE = "Vin's Blog"
 # This is the main URL for your site. It will be used
 # in a prominent link
-SITE_URL = "/"
+SITE_URL = "http://carlosvin.github.io/"
 # This is the URL where nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "http://getnikola.com/"
