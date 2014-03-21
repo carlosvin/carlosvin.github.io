2a8dc2f4c51db948221c1317feb05960d2ed3363
Carlos <carlosvin@gmail.com>
2014-03-21 16:23:04 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -2,20 +2,17 @@
 from __future__ import unicode_literals
 
 AUTHOR = u"Carlos Martín Sánchez"
-SITENAME = u"Carlosvin's Blog"
+SITENAME = u"Vin's Blog"
 SITEURL = 'http://carlosvin.github.io'
 TIMEZONE = u"Europe/Madrid"
 
-# can be useful in development, but set to False when you're ready to publish
-RELATIVE_URLS = True
 
 GITHUB_URL = 'http://github.com/carlosvin/'
 DISQUS_SITENAME = u"carlosvin"
-#PDF_GENERATOR = True
-#REVERSE_CATEGORY_ORDER = True
+PDF_GENERATOR = True
+REVERSE_CATEGORY_ORDER = True
 DEFAULT_PAGINATION = 5
 
-#LOCALE = ['es_ES', 'en_US', 'es', 'usa']
 DEFAULT_LANG = 'es'
 
 FEED_ALL_RSS = 'feeds/all.rss.xml'
@@ -42,11 +39,4 @@ STATIC_PATHS = [
     'extra/robots.txt',
     ]
 
-# custom page generated with a jinja2 template
-#TEMPLATE_PAGES = {'pages/jinja2_template.html': 'jinja2_template.html'}
-
-# code blocks with line numbers
-PYGMENTS_RST_OPTIONS = {'linenos': 'table'}
-
 GOOGLE_ANALYTICS = u"UA-1328360-9"
-
