bc54b68a756b66ff7b6d72c10d911e58c4517427
Carlos <carlosvin@gmail.com>
2014-06-27 14:02:21 +0200

    added metadata

--- conf.py
+++ conf.py
@@ -608,7 +608,10 @@ $(document).ready(function() {
 # FILE_METADATA_REGEXP = None
 
 # Additional metadata that is added to a post when creating a new_post
-# ADDITIONAL_METADATA = {}
+ADDITIONAL_METADATA = { 
+    'google-site-verification' : 'XzkYCjSblA4sHLJDa1Higd_O6P8hudmo6YJRDp5dDDc',
+    'author': 'Carlos Martín Sánchez',
+}
 
 # Nikola supports Twitter Card summaries / Open Graph.
 # Twitter cards make it possible for you to attach media to Tweets
