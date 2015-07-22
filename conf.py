2239fc600790a984f6d30b817732171f586aec8d
Carlos Martin Sanchez <carlosvin@gmail.com>
2015-07-22 23:50:24 +0200

    fixing config

--- conf.py
+++ conf.py
@@ -649,7 +649,7 @@ $(document).ready(function() {
 TIMEZONE = 'Europe/Madrid'
 
 # If webassets is installed, bundle JS and CSS to make site loading faster
-# USE_BUNDLES = True
+USE_BUNDLES = True
 
 # Plugins you don't want to use. Be careful :-)
 # DISABLED_PLUGINS = ["render_galleries"]
