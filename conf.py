3cb0dab9837395e27a9f4ba0426f4366f1ebd383
Carlos Martin Sanchez <carlosvin@gmail.com>
2017-09-16 14:39:00 +0200

    Add about section and switch to diqus comments

--- conf.py
+++ conf.py
@@ -375,12 +375,12 @@ CONTENT_FOOTER = CONTENT_FOOTER.format(email=BLOG_EMAIL,
 # To use comments, you can choose between different third party comment
 # systems, one of "disqus", "livefyre", "intensedebate", "moot",
 #                 "googleplus", "facebook" or "isso"
-COMMENT_SYSTEM = "googleplus"
+COMMENT_SYSTEM = "disqus"
 # And you also need to add your COMMENT_SYSTEM_ID which
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = "carlosvin@gmail.com"
+COMMENT_SYSTEM_ID = "carlosvin"
 
 # Enable annotations using annotateit.org?
 # If set to False, you can still enable them for individual posts and pages
