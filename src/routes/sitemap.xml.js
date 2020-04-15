51e9f4d81949d43db76c6a2770c7f777f976ea2b
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-04-15 23:02:40 +0200

    generate only one sitemap with translations instead of sitemapindex

---
+++ src/routes/sitemap.xml.js
@@ -0,0 +1,39 @@
+import { url } from '../services/models';
+import { store } from '../store';
+import { BASE_URL } from '../conf';
+
+const fs = require('fs');
+
+const pages = [""];
+
+fs.readdirSync("./src/routes").forEach(file => {
+	file = file.split('.')[0];
+	if (file.charAt(0) !== '_' && !file.startsWith("sitemap") && file !== "index") {
+		pages.push(file);
+	}
+});
+
+const render = (pages, posts) => `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+	${pages
+		.map(page => `<url><loc>${BASE_URL}/${page}</loc><priority>0.85</priority></url>`)
+		.join("\r\n")}
+  	${posts.map(({ slug, lang, date, otherLangs}) => `
+		<url>
+		<loc>${url({ slug, lang })}</loc>
+		<priority>0.69</priority>
+		<lastmod>${date}</lastmod>
+		${otherLangs ? otherLangs.map(l => `<xhtml:link 
+				rel="alternate"
+				hreflang="${l}"
+				href="${url({ slug, lang: l })}"/>`) : ''
+			}
+		</url>`).join("\r\n")}
+</urlset>`;
+
+export function get(req, res, next) {
+	res.setHeader("Cache-Control", `max-age=0, s-max-age=${1800}`); // 30 minutes
+	res.setHeader("Content-Type", "application/rss+xml");
+	const sitemap = render(pages, store.index);
+	res.end(sitemap);
+}
--- src/services/models.js
+++ src/services/models.js
@@ -1,5 +1,9 @@
-import { BASE_BLOG_PATH } from '../conf';
+import { BASE_BLOG_PATH, BASE_URL } from '../conf';
 
 export function path({slug, lang}) {
     return `${BASE_BLOG_PATH}/${slug}/${lang}`;    
+}
+
+export function url(post){
+    return `${BASE_URL}${path(post)}`;
 }
