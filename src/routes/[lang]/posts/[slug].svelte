c920b9576eed9b5fd7dcae48156f57618ade66bf
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-05-01 03:34:23 +0200

    keep responding to old blog paths

---
+++ src/routes/[lang]/posts/[slug].svelte
@@ -0,0 +1,8 @@
+<script context="module">
+  import { path } from "../../../services/models";
+
+  export async function preload({ params, query }) {
+    const {slug, lang} = params;
+    this.redirect(302, path(slug, lang));
+  }
+</script>
