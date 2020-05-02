f4722f81e0f6f580c6487539b2dd5c136121d299
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-05-03 00:40:06 +0200

    use a number of columns dependeing on device screen view size

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -3,10 +3,22 @@
   ul {
     margin: 0 0 1em 0;
     line-height: 1.5;
-    display: flex;
-    flex-wrap: wrap;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
     list-style-type: none;
     padding: 0;
+  }
+
+  @media screen and (max-width: 1200px) {
+    ul {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+
+  @media screen and (max-width: 840px) {
+    ul {
+      display: block;
+    }
   }
 </style>
 
