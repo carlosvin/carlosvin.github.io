f8ee583fb69a39a61731ef8b970703d486cbb48e
Carlos Martin Sanchez <carlmartin@ebay.com>
2020-07-30 15:28:14 +0200

    fix integration tests, use web browser language to select the page title

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,33 +1,41 @@
 describe('Index page', () => {
 	beforeEach(() => {
 		cy.visit('/')
-	});
+	})
 
 	it('has the correct <h1>', () => {
 		cy.contains('h1', 'Recent posts')
-	});
+	})
 
-});
+})
 
 describe('Navigation bar', () => {
 	beforeEach(() => {
 		cy.visit('/')
-	});
+	})
 
 	function assertNav(name, path) {
-		cy.get('nav a').contains(name).click();
-		cy.url().should('include', path);
+		cy.get('nav a').contains(name).click()
+		cy.url().should('include', path)
+	}
+
+	function getSiteName () {
+		if (navigator.language.startsWith('es')) {
+			return 'Carlos dice bla bla'
+		} else {
+			return 'Carlos says bla bla'
+		}
 	}
 
 	it('navigates to about > home > categories', () => {
-		assertNav('About', '/about');
-		assertNav('Carlos says bla bla', '/');
-		assertNav('Categories', '/categories');
+		assertNav('About', '/about')
+		assertNav(getSiteName(), '/')
+		assertNav('Categories', '/categories')
 	});
 
 	it('navigates to categories > home > about', () => {
-		assertNav('Categories', '/categories');
-		assertNav('Carlos says bla bla', '/');
-		assertNav('About', '/about');
+		assertNav('Categories', '/categories')
+		assertNav(getSiteName(), '/')
+		assertNav('About', '/about')
 	});
 });
