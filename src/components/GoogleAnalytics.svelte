e0c72174a93d3691059dac6e51d05a0d6008c7b1
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-08-01 21:29:01 +0200

    use analytics instead of tag manager

--- src/components/GoogleAnalytics.svelte
+++ src/components/GoogleAnalytics.svelte
@@ -1,32 +1,21 @@
 <svelte:head>
-    <script defer src="https://www.googletagmanager.com/gtag/js?id={id}"></script>
+    <script src="https://www.google-analytics.com/analytics.js" async></script>
 </svelte:head>
 <script>
     export let stores;
     export let id;
 
     if (typeof window !== "undefined") {
-        window.dataLayer = window.dataLayer || [];
-        window.gtag = function gtag() {
-            window.dataLayer.push(arguments);
-        }
-        window.gtag("js", new Date());
-        window.gtag("config", id, { 
-            'send_page_view': false, 
-            transport_type: 'beacon', 
-        });
+        window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+        ga('create', id, 'auto');
+        ga('set', 'transport', 'beacon'); 
     }
 
     const { page } = stores();
     $: {
-        if (typeof gtag !== "undefined"){
-            window.gtag('event', 'page_view', {
-                page_title: document.title,
-                // page_location: '<Page Location>',
-                page_path: $page.path,
-                send_to: id,
-                // useBeacon: true
-            });
+        if (typeof ga !== "undefined"){
+            ga('set', 'page', $page.path);
+            ga('send', 'pageview');
         }
     }
 </script>
