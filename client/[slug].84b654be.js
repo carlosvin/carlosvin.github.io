1e5ce67b128f64f22bfaf47c2419d87d5e9e7696
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-08-03 20:07:25 +0200

    Updates

---
+++ client/[slug].84b654be.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as r}from"./client.119d774d.js";import{p as e}from"./url.7ee1e325.js";async function a({params:s}){const{slug:t,lang:r}=s;this.redirect(302,e(t,r))}export default class extends s{constructor(s){super(),t(this,s,null,null,r,{})}}export{a as preload};
