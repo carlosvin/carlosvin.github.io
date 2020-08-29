24d0b9284a58f4e7cde15ffeceacb255d1f44a71
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-08-29 22:25:52 +0200

    Add description meta tag

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,8 @@
 <svelte:head>
-	<title>About</title>
+	<title>About me</title>
+	<meta name="description" content="A little bit about me" />
 </svelte:head>
 
 <h1>About me</h1>
 
-<p>I am Software Engineer, my career has been mainly focused on developing distributed systems although it hasn't been focused in a specific technology. I think it is more important to choose the right technology for each use case.</p>
+<p>I am Software Engineer, my career has been mainly focused on developing distributed systems although it hasn't been focused in a specific technology, I think it is more important to choose the right technology for each use case.</p>
