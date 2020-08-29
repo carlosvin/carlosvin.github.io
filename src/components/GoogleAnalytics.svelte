9cda53f7760e97d053e3104625947f71d45fb2cf
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-08-30 01:51:39 +0200

    Add workaround to avoid the svelte bug loading twice the head scripts. Send only analytics in production built

--- src/components/GoogleAnalytics.svelte
+++ src/components/GoogleAnalytics.svelte
@@ -1,26 +1,41 @@
-<script>
-    const url = 'https://www.google-analytics.com/analytics.js';
-    export let stores: () => any;
-    export let id: string;
-
+<script lang="ts">
+  export let stores: () => any;
+  export let id: string;
+  const url = "https://www.google-analytics.com/analytics.js";
+  const env = process.env.NODE_ENV;
+  const isProd = env === undefined || env === "production";
+  if (isProd) {
     if (typeof window !== "undefined") {
-        window.ga=window.ga||function(){
-            (window.ga.q=window.ga.q||[]).push(arguments)
+      window.ga =
+        window.ga ||
+        function () {
+          (window.ga.q = window.ga.q || []).push(arguments);
         };
-        window.ga.l=+new Date;
-        window.ga('create', id, 'auto');
-        window.ga('set', 'transport', 'beacon'); 
+      window.ga.l = +new Date();
+      window.ga("create", id, "auto");
+      window.ga("set", "transport", "beacon");
     }
-
-    const { page } = stores();
-    $: {
-        if (typeof ga !== "undefined"){
-            window.ga('set', 'page', $page.path);
-            window.ga('send', 'pageview');
-        }
+  }
+  const { page } = stores();
+  $: {
+    if (typeof ga !== "undefined" && isProd) {
+      window.ga("set", "page", $page.path);
+      window.ga("send", "pageview");
     }
+  }
+  let isLoaded = false;
+  function shouldLoad () {
+      if (isProd && isLoaded === false) {
+          isLoaded = true;
+          return true;
+      }
+      return false;
+  }
 </script>
+
 <svelte:head>
-	<link href={url} rel="preload" as="script">
+  {#if shouldLoad()}
+    <link href={url} rel="preload" as="script" />
     <script src={url} async></script>
+  {/if}
 </svelte:head>
