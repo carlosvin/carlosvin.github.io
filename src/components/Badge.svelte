e5d459df3740639d472ead6649dd36c41e42f9ce
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-08-30 23:20:33 +0200

    Badge might end up inside an "a" html tag, which is not valid html

--- src/components/Badge.svelte
+++ src/components/Badge.svelte
@@ -2,7 +2,8 @@
 export let hover = true;
 </script>
 <style>
-div {
+span {
+    display: block;
     border-radius: 0.2em;
     padding: 0 0.3em 0 0.3em;
     box-shadow: 0.0em 0.0em 0.1em #777;
@@ -17,6 +18,6 @@ div {
 }
 </style>
 
-<div class={hover ? 'hover' : undefined}>
+<span class={hover ? 'hover' : undefined}>
     <slot></slot>
-</div>
+</span>
