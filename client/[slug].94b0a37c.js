77a8abf7babbc998c572d6b3819ddff236fbbd3e
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-08-30 23:22:37 +0200

    Updates

---
+++ client/[slug].94b0a37c.js
@@ -0,0 +1,1 @@
+import{S as s,i as r,s as t}from"./client.a333a991.js";import{p as a}from"./url.256ef215.js";function e({params:s}){const{slug:r,lang:t}=s;return this.redirect(301,a(r,t))}export default class extends s{constructor(s){super(),r(this,s,null,null,t,{})}}export{e as preload};
