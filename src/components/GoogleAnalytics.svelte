cdaa00cf97fb2e03ab4031ba84ce0901418f8154
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-09-10 23:40:13 +0200

    remove unused url

--- src/components/GoogleAnalytics.svelte
+++ src/components/GoogleAnalytics.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   export let stores: () => any;
-  const url = "https://www.google-analytics.com/analytics.js";
   const env = process.env.NODE_ENV;
   const isProd = env === undefined || env === "production";
   const { page } = stores();
