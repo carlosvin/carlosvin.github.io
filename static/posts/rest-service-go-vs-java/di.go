a46ab8e5a3f3d442460d69a04764d4ef065fdb55
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-11-01 21:25:44 +0100

    Use new rollup-plugin-asciidoc which supports include directive

--- static/posts/rest-service-go-vs-java/di.go
+++ static/posts/rest-service-go-vs-java/di.go
@@ -9,19 +9,19 @@ type Greeter interface {
 
 type greeterHello struct{}
 
-func (g *greeterHello) greet() {
+func (g *greeterHello) greet() { // <3>
 	fmt.Println("Hello!")
 }
 
 type greeterHi struct{}
 
-func (g *greeterHi) greet() {
+func (g *greeterHi) greet() { // <4>
 	fmt.Println("Hi!")
 }
 
 // App Application representation
 type App struct {
-	greeters []Greeter
+	greeters []Greeter // <1>
 }
 
 func (app *App) startup() {
@@ -31,7 +31,7 @@ func (app *App) startup() {
 }
 
 func main() {
-	greeters := []Greeter{
+	greeters := []Greeter{ // <2>
 		&greeterHello{},
 		&greeterHi{},
 		&greeterHello{}}
@@ -40,3 +40,10 @@ func main() {
 
 	app.startup()
 }
+
+/*
+<1> `App` accepts an array of `Greeter`
+<2> During `App` instantiation we pass different implementations of `Greeter`
+<3> Greeter implementation that prints *Hello!*
+<4> Greeter implementation that prints *Hi!*
+*/
