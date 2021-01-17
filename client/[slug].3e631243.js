68707505515feac406046dc58c5cace98181cea4
Carlos Martin Sanchez <carlosvin@gmail.com>
2021-01-17 23:02:09 +0100

    Updates

--- client/[slug].3e631243.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as r,s as t}from"./client.42b97985.js";import{p as e}from"./url.f152c0cf.js";function l({params:s}){const{slug:r,lang:t}=s;return this.redirect(301,e(r,t))}export default class extends s{constructor(s){super(),r(this,s,null,null,t,{})}}export{l as preload};
