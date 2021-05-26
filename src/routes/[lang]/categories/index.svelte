ab8ee4f3472ff22f44e238cf0ac28a5502b409a2
Carlos Martin Sanchez <carlosvin@gmail.com>
2021-05-26 23:12:26 +0200

    remove warning passing unnecessary property

--- src/routes/[lang]/categories/index.svelte
+++ src/routes/[lang]/categories/index.svelte
@@ -1,47 +1,65 @@
 <script context="module" lang="ts">
-    // import { jsonLdCategories, jsonLdScript} from "../../services/jsonld";
-    import type { IndexResponse, Category } from "$lib/models/interfaces";
+	// import { jsonLdCategories, jsonLdScript} from "../../services/jsonld";
+	import type { IndexResponse, Category } from '$lib/models/interfaces';
 
-    export async function load({fetch, page}) {
-      const lang = page.params.lang;
-      const resp: IndexResponse<Category> = await (await fetch(`/${lang}/categories/json`)).json();
-      // TODO const jsonLd = jsonLdScript(jsonLdCategories(categories, title, description, request.path));
-      resp.title = `${resp.title} - Categories`;
-      resp.description = "Index of blog categories";
+	interface Resp {
+		props: {
+			index: Category[];
+			title: string;
+			description: string;
+			lang: string;
+		};
+	}
 
-      return { props: {...resp, lang} };
-	  }
-  </script>
-  
-  <script lang="ts">
-    import { categoryPath } from "$lib/services/url";
+	export async function load({ fetch, page }): Promise<Resp> {
+		const lang = page.params.lang;
+		const resp: IndexResponse<Category> = await (await fetch(`/${lang}/categories/json`)).json();
+		// TODO const jsonLd = jsonLdScript(jsonLdCategories(categories, title, description, request.path));
+		resp.title = `${resp.title} - Categories`;
+		resp.description = 'Index of blog categories';
 
-    import Index from "$lib/components/Index.svelte";
-    import Entry from "$lib/components/Entry.svelte";
-    
-    export let index: Category[];
-    export let title: string;
-    export let description: string;
-    export let lang: string;
-  </script>
-  
-  <svelte:head>
-    <title>{title}</title>
-    <meta name="description" content={description} />
-  </svelte:head>
-  
-  <h1>Categories</h1>
-  {#if index}
-  <Index>
-    {#each index as c}
-      <Entry>
-        <a sveltekit:prefetch href={categoryPath(c.slug, lang)}>{c.name}</a>
-      </Entry>
-    {/each}
-    <Entry>
-      <a href="/posts">All posts</a>
-    </Entry>
-  </Index>
-  {:else}
-  <p>Loading...</p>
-  {/if}
+		return {
+			props: {
+				title: resp.title,
+				index: resp.index,
+				description: resp.description,
+				lang
+			}
+		};
+	}
+
+</script>
+
+<script lang="ts">
+	import { categoryPath } from '$lib/services/url';
+
+	import Index from '$lib/components/Index.svelte';
+	import Entry from '$lib/components/Entry.svelte';
+
+	export let index: Category[];
+	export let title: string;
+	export let description: string;
+	export let lang: string;
+
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+</svelte:head>
+
+<h1>Categories</h1>
+{#if index}
+	<Index>
+		{#each index as c}
+			<Entry>
+				<a sveltekit:prefetch href={categoryPath(c.slug, lang)}>{c.name}</a>
+			</Entry>
+		{/each}
+		<Entry>
+			<a href="/posts">All posts</a>
+		</Entry>
+	</Index>
+{:else}
+	<p>Loading...</p>
+{/if}
