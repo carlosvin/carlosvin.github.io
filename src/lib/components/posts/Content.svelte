ca2bf663f12061e0581e7c7a3176050849e9548c
Carlos Martin Sanchez <carlosvin@gmail.com>
2021-06-24 23:47:29 +0200

    improving content styles

--- src/lib/components/posts/Content.svelte
+++ src/lib/components/posts/Content.svelte
@@ -1,95 +1,94 @@
 
 <style global>
-  .content h2 {
+  .postContent h2 {
     font-size: 1.6rem;
     font-weight: 500;
     margin-top: 1.2em;
     vertical-align: middle;
   }
 
-  .content h2::after,
-  .content h3::after,
-  .content h4::after,
-  .content h5::after,
-  .content h6::after {
+  .postContent h2::after,
+  .postContent h3::after,
+  .postContent h4::after,
+  .postContent h5::after,
+  .postContent h6::after {
     content: " <";
     color: #bbb;
     font-weight: lighter;
   }
 
-  .content h2:hover::after,
-  .content h3:hover::after,
-  .content h4:hover::after,
-  .content h5:hover::after,
-  .content h6:hover::after {
+  .postContent h2:hover::after,
+  .postContent h3:hover::after,
+  .postContent h4:hover::after,
+  .postContent h5:hover::after,
+  .postContent h6:hover::after {
     color: #669;
   }
   
-  .content h3::after {
+  .postContent h3::after {
     content: " <<";
   }
 
-  .content h4::after {
+  .postContent h4::after {
     content: " <<<";
   }
 
-  .content h5::after {
+  .postContent h5::after {
     content: " <<<<";
   }
 
-  .content h6::after {
+  .postContent h6::after {
     content: " <<<<<";
   }
 
-  .content h3 {
+  .postContent h3 {
     font-size: 1.4rem;
     margin-top: 1em;
   }
 
-  .content h4 {
+  .postContent h4 {
     font-size: 1.2rem;
     margin-top: 0.8em;
   }
 
-  .content h5 {
+  .postContent h5 {
     font-size: 1rem;
     margin-top: 0.6em;
   }
 
-  .content h6 {
+  .postContent h6 {
     font-size: 0.8rem;
     margin-top: 0.4em;
   }
-  .content pre {
+  .postContent pre {
     background-color: #f9f9f9;
-    box-shadow: inset 1px 1px 5px rgba(0, 0, 0, 0.05);
     padding: 0.5em;
     border-radius: 2px;
     overflow-x: auto;
   }
 
-  .content pre, 
-  .content code {
+  .postContent pre, 
+  .postContent code {
     background-color: transparent;
     padding: 0;
   }
 
-  .content ul {
+  .postContent ul {
     line-height: 1.5;
   }
 
-  .content li {
+  .postContent li {
     margin: 0 0 0.5em 0;
   }
-  .content dt {
+  .postContent dt {
     font-weight: bold;
   }
 
-  .content .footnote {
+  .postContent .footnote {
     font-size: smaller;
   }
 
-  .content .admonitionblock {
+  .postContent .admonitionblock {
     background: #e9e9e9;
     padding: 1em 0.5em;
     border-radius: 0.2em;
@@ -97,11 +96,11 @@
     font-size: 1.1em;
   }
 
-  .content .admonitionblock .content a {
-    word-break: break-all;
+  .postContent .admonitionblock .content a {
+    word-break: break-word;
   }
 
-  .content .admonitionblock td.icon {
+  .postContent .admonitionblock td.icon {
     font-size: 3em;
     cursor: default;
     text-align: center;
@@ -119,133 +118,143 @@
   }
 
   @media screen and (max-width: 480px) {
-    .content .admonitionblock td.icon {
+    .postContent .admonitionblock td.icon {
       font-size: 1em;
     }
 
-    .content .toc {
+    .postContent .toc {
       font-size: 1em;
     }
 
-    .content .imageblock img {
+    .postContent .imageblock img {
       width: 90%;
     }
   }
 
-  .content .dlist dd > p  {
+  .postContent .dlist dd > p  {
     margin: 0 auto;
   }
 
-  .content .dlist dl {
+  .postContent .dlist dl {
     margin: 0 auto;
   }
 
-  .content .toc ul {
+  .postContent .toc ul {
     padding-right: 1em;
     padding-left: 2em;
   }
 
-  .content .toc .sectlevel1 {
+  .postContent .toc .sectlevel1 {
     margin-block-start: 0.4em;
   }
 
-  .content .imageblock img {
+  .postContent .imageblock img {
     object-fit:cover;
   }
 
-  .content .admonitionblock .icon > .title {
+  .postContent .admonitionblock .icon > .title {
     font-size: small;
     font-weight: lighter;
     font-family: Roboto, -apple-system, BlinkMacSystemFont, Segoe UI, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
 
-  .content .admonitionblock.note .icon::before {
+  .postContent .admonitionblock.note .icon::before {
     color: #19407c;
     content: "\e900";
   }
-  .content .admonitionblock.tip .icon::before {
+  .postContent .admonitionblock.tip .icon::before {
     color: rgb(6, 143, 148);
     content: "\e904";
   }
   
-  .content .admonitionblock.warning .icon::before {
+  .postContent .admonitionblock.warning .icon::before {
     content: "\e906";
     color: #bf6900;
   }
-  .content .admonitionblock.caution .icon::before {
+  .postContent .admonitionblock.caution .icon::before {
     content: "\e906";
     color: #bf3400;
   }
-  .content .admonitionblock.important .icon::before {
+  .postContent .admonitionblock.important .icon::before {
     content: "\e902";
     color: rgb(26, 180, 218);
   }
-  .content .stretch {
+  .postContent .stretch {
     width: 100%;
   }
-  .content .center {
+  .postContent .center {
     text-align: center;
   }
-  .content .caption {
+  .postContent .caption {
     color: gray;
   }
-  .content .hdlist td, .hdlist p {
+  .postContent .hdlist td, .hdlist p {
     vertical-align: top;
   }
-  .content .hdlist p {
+  .postContent .hdlist p {
     display: table-cell;
   }
-  .content .hdlist .hdlist1 {
+  .postContent .hdlist .hdlist1 {
     font-weight: bold;
   }
-  .content .toc {
+  .postContent .toc {
     font-size: 1.2em;
   }
-  .content .toc li {
+  .postContent .toc li {
     font-size: smaller;
   }
   
-  .content :global(.listingblock) {
+  .postContent .listingblock {
     line-height: 1.2em;
     margin-bottom: 0.8em;
     border: 1px #bbb dashed;
     background: rgba(0, 0, 0, 0.05);
   }
-  .content .listingblock .title {
-    font-size: 1em;
-    padding: 0.1em 0.5em;
+  .postContent .listingblock .title {
+    font-size: 0.9em;
     border-bottom: 1px #bbb dashed;
+    padding: 0.2em;
   }
-  .content .hljs-comment {
+  .postContent .listingblock .content {
+    background-color: #f9f9f9;
+    padding: 0.2em;
+  }
+  .postContent .hljs-comment {
     color: #777070 !important; 
   }
-  .content .hljs-meta-string {
+  .postContent .hljs-meta-string {
     color: #1f7199 !important; 
   }
-  .content :global(pre.highlight) {
-    margin: 0.2em 0 0 0;
-    overflow: auto;
-    
+  .postContent pre.highlight {
+    margin: 0.4em 0 0.4em 0.4em;
+    overflow: auto;    
   }
-  .content .colist p {
+  .postContent .colist p {
     margin: 0;
   }
-  .content .colist li {
+  .postContent .colist li {
     margin: 0;
   }
-  .content .tableblock {
+  .postContent .tableblock {
     margin-top: 0.5em;
     margin-bottom: 0.5em;
   }
-  .content .colist ol {  
+  .postContent .colist ol {  
     margin: 0.1em 0.6em;
     margin-inline-start: 0.6em;
     padding-inline-start: 0.6em;    
   }
+
+  .postContent .conum,
+  .postContent .colist li {
+    color: #669;
+    font-family: Roboto, -apple-system, BlinkMacSystemFont, Segoe UI, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-weight: normal;
+  }
 </style>
 
-<div class='content'>
+<div class='postContent'>
     <slot>
     </slot>
 </div>
