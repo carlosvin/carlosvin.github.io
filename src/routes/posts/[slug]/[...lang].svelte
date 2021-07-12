429b440ee55ed8c34d9d7a511428ff34c94a8f8e
Carlos Martin Sanchez <carlosvin@gmail.com>
2021-07-12 18:35:43 +0200

    keep legacy routes

---
+++ src/routes/posts/[slug]/[...lang].svelte
@@ -0,0 +1,16 @@
+<script context="module">
+	import { DEFAULT_LANG } from '$lib/conf';
+	import { postPath } from '$lib/services/url';
+
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	export async function load({ page }) {
+		const {slug, lang} = page.params;
+		
+		return {
+			status: 301, //permanent redirect
+			redirect: postPath(slug, lang || DEFAULT_LANG)
+		};
+	}
+</script>
