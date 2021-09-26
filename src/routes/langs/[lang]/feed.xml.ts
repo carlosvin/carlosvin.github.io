567d8606fea0374915bff0beef210ac1643a3ecb
Carlos Martin Sanchez <carlosvin@gmail.com>
2021-09-26 12:34:20 +0200

    fix accessing to langs store

--- src/routes/langs/[lang]/feed.xml.ts
+++ src/routes/langs/[lang]/feed.xml.ts
@@ -1,19 +1,18 @@
 import { Atom } from '$lib/services/atom';
 import { blogStore } from '$lib/stores/blog';
 import { i18n } from '$lib/stores/lang';
-import {get as g} from 'svelte/store';
 
 export function get({ params }) {
 	const { lang } = params;
 	const posts = blogStore.getByLang(lang);
 	
 	const { xml } = new Atom(
-		g(i18n).get('siteName'),
-		g(i18n).get('siteDescription'),
+		i18n.get('siteName'),
+		i18n.get('siteDescription'),
 		posts,
 		import.meta.env.VITE_BASE_URL,
 		'',
-		g(i18n).get('author')
+		i18n.get('author')
 	);
 
 	return {
