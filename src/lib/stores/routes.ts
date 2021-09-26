c972bd5e812236791c88f311a36728f1f1186f87
Carlos Martin Sanchez <carlosvin@gmail.com>
2021-09-26 12:47:32 +0200

    remove file extension

--- src/lib/stores/routes.ts
+++ src/lib/stores/routes.ts
@@ -3,9 +3,10 @@ import path from 'path';
 const BASE_PATH = '/src/routes';
 
 export const routes = Object.keys(import.meta.globEager('/src/routes/**/*.svelte'))
-	.filter((file) => isValid(file))
-	.map((file) => removeIndex(file))
-	.map((file) => path.relative(BASE_PATH, file));
+	.filter(file => isValid(file))
+	.map(file => removeIndex(file))
+	.map(file => path.relative(BASE_PATH, file))
+	.map(file => file.replace('.svelte', ''));
 
 function isValid(file: string) {
 	const name = path.basename(file, '.svelte');
