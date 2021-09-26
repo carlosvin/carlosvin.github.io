ccaa81d339929d7dd494846fe92523d795b897da
Carlos Martin Sanchez <carlosvin@gmail.com>
2021-09-26 12:56:30 +0200

    Revert "Updates"
    This reverts commit 53a5361a06213a3c0dcada722f2e137896175a08.

--- _app/chunks/url-bb89230a.js
+++
@@ -1,1 +0,0 @@
-const n="en";function s(n,s){return`/langs/${s}/categories/${n}`}function t(n,s){return`/langs/${s}/posts/${n}`}export{n as D,s as c,t as p};
---
+++ _app/pages/posts/[slug]/[...lang].svelte-e04dec64.js
@@ -0,0 +1,1 @@
+import{S as s,i as a,s as r}from"../../../chunks/vendor-aa34231d.js";import{p as t}from"../../../chunks/url-28a36952.js";import{D as n}from"../../../chunks/conf-318ca8e3.js";function o({page:s}){const{slug:a,lang:r}=s.params;return{status:301,redirect:t(a,r||n)}}class u extends s{constructor(s){super(),a(this,s,null,null,r,{})}}export{u as default,o as load};
