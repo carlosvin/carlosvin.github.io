693a7f95b7a908e147323f738aa85e033bba00fc
Carlos Martin Sanchez <carlosvin@gmail.com>
2022-01-08 23:54:16 +0100

    Updates

--- _app/chunks/conf-318ca8e3.js
+++
@@ -1,1 +0,0 @@
-const s="posts",a="categories",e="en";export{s as B,a as C,e as D};
---
+++ _app/chunks/url-bb89230a.js
@@ -0,0 +1,1 @@
+const s="posts",o="categories",a="en";function c(t,n){return`/langs/${n}/${o}/${t}`}function e(t,n){return`/langs/${n}/${s}/${t}`}export{a as D,c,e as p};
