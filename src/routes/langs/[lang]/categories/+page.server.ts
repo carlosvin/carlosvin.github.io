b5e3fbd11f3e30ab845a42b1cb0002a94dbe3871
Carlos Martin Sanchez <carlos.martin-sanchez@mongodb.com>
2024-08-25 14:34:35 +0200

    add categories index

---
+++ src/routes/langs/[lang]/categories/+page.server.ts
@@ -0,0 +1,11 @@
+
+import { blogStore } from '$lib/stores/blog';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = ({ params }) => {
+    return {
+        index: [...blogStore.categories.values()],
+        langs: blogStore.langs,
+        lang: params.lang
+    };
+};
